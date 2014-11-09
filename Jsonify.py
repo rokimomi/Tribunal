000dfe2249feed22a5adc0ebeece0b9f6920fa85
rokimomi <rokimomi@gmail.com>
2014-11-09 00:05:40 -0500

    Fixed memory issues in jsonify by adding "extract()" to all beautiful soup string lines so that the strings I needed were decoupled from the larger page structure they came from.

--- Jsonify.py
+++ Jsonify.py
@@ -6,6 +6,17 @@ from bs4 import BeautifulSoup as bs
 import re
 import json
 import glob
+import gc
+import sys
+
+from pympler import muppy
+from pympler import summary
+
+all_objects = muppy.get_objects()
+sum1 = summary.summarize(all_objects)
+summary.print_(sum1)
+
+
 # 6030355 SHOWS A "DID NOT LOAD CORRECTLY" MESSAGE FOR GAMES 2 and 3
 
 data = {"tribunal":[]}
@@ -28,6 +39,11 @@ for file in files_list:
     #if current_file_num == num_files:
     #    break
 
+    if current_file_num % 25 == 0:
+        all_objects = muppy.get_objects()
+        sum1 = summary.summarize(all_objects)
+        summary.print_(sum1)
+
     current_file_num += 1
 
     print str(current_file_num) + ' / ' + str(num_files) + ' (%.2f' % (current_file_num/float(num_files)*100) + '%)'
@@ -42,30 +58,33 @@ for file in files_list:
 
         soup = bs(html)
 
-        caseNum = soup.find_all("span", class_=re.compile('raw-case-number'))[0].contents[0]
+        caseNum = soup.find_all("span", class_=re.compile('raw-case-number'))[0].contents[0].extract()
 
-        reports = soup.find_all("span", class_=re.compile('total-reports-fill'))[0].contents[0]
-        games = soup.find_all("span", class_=re.compile('total-games-fill'))[0].contents[0]
-        decision = soup.find_all("p", class_=re.compile('verdict-stat'))[2].contents[0]
-        agreement = soup.find_all("p", class_=re.compile('agreement'))[0].contents[0]
-        punishment = soup.find_all("p", class_=re.compile('verdict-stat'))[0].contents[0]
+        reports = soup.find_all("span", class_=re.compile('total-reports-fill'))[0].contents[0].extract()
+        games = soup.find_all("span", class_=re.compile('total-games-fill'))[0].contents[0].extract()
+        decision = soup.find_all("p", class_=re.compile('verdict-stat'))[2].contents[0].extract()
+        agreement = soup.find_all("p", class_=re.compile('agreement'))[0].contents[0].extract()
+        punishment = soup.find_all("p", class_=re.compile('verdict-stat'))[0].contents[0].extract()
+
 
         ####
 
-        games_div = soup.find_all("div", class_=re.compile('tab-content-container'))[0]
+        games_div = soup.find_all("div", class_=re.compile('tab-content-container'))[0].extract()
 
         recentGames = []
 
         # cycle through the games this player was reported in
 
+
+
         for i in range(1, int(games)+1):
 
-            game = games_div.find_all("div", id=re.compile('game'+str(i)))[0]
+            game = games_div.find_all("div", id=re.compile('game'+str(i)))[0].extract()
 
             # basic game metadata
-            gameType = game.find_all("p", id=re.compile('stat-type-fill'))[0].contents[0]
-            gameLength = game.find_all("p", id=re.compile('stat-length-fill'))[0].contents[0]
-            outcome = game.find_all("p", id=re.compile('stat-outcome-fill'))[0].contents[0]
+            gameType = game.find_all("p", id=re.compile('stat-type-fill'))[0].contents[0].extract()
+            gameLength = game.find_all("p", id=re.compile('stat-length-fill'))[0].contents[0].extract()
+            outcome = game.find_all("p", id=re.compile('stat-outcome-fill'))[0].contents[0].extract()
 
             recentGames.append({"gameType": gameType, "gameLength": gameLength, "outcome": outcome})
 
@@ -75,18 +94,30 @@ for file in files_list:
         # add this case to larger json
         data["tribunal"].append(case)
 
+        soup.decompose()
+
+        gc.collect()
+
+        html.close()
+
+
+
     except IndexError, e:
         print "IndexError: case " + caseNum + ". Throwing case out..."
         num_files_thrown += 1
+        soup.decompose()
+        gc.collect()
     except UnicodeDecodeError, e:
         print "UnicodeDecodeError: case " + caseNum + ". Throwing case out..."
         num_files_thrown += 1
+        soup.decompose()
+        gc.collect()
 
 # save data
 with open('./json/data.json', 'w') as outfile:
     json.dump(data, outfile)
 
-print 'Processed ' + str(num_files) + 'total cases.'
+print 'Processed ' + str(num_files) + ' total cases.'
 print str(num_files_thrown) + ' cases thrown out ('+ str((num_files_thrown/float(num_files)*100)) +'%)'
 print str(num_files - num_files_thrown) + ' remaining (' + str(((num_files-num_files_thrown)/float(num_files)*100)) + '%)'
 
