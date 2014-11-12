c3dcd8c1b1edf3f5d7d27adf94e3a7ec58abca15
rokimomi <rokimomi@gmail.com>
2014-11-11 20:32:10 -0500

    added functionality to Stats.py

--- Stats.py
+++ Stats.py
@@ -1,23 +1,20 @@
 __author__ = 'amine'
 
 import json
+import math
 from pprint import pprint
 
 # basic stats questions, answer by combing through json
 
 ###
 
-# how many reported players are punished, how many pardoned
+# average length of games?
 
-# what's the win rate of reported players
+# what's the kda of reported players?
 
-# what's the kda of reported players
-
-# what percent of reported players cuss
+# what percent of reported players cuss?
 
 # what's the message rate of the reported player compared to the rest of the game?
-
-# average length of games?
 
 # average games of times below 21 minutes (20 mins is minimum for surrendering, allow one minute for surrendering)
 
@@ -25,10 +22,27 @@ from pprint import pprint
 
 # are the dominion versions of the above different at all?
 
-# what are the possible decisions/agreements/punishments, how often do they show up
-
 ####
 
+# todo what about hours encoding
+def time_to_seconds(time):
+    hms = time.split(':')
+
+    if(len(hms) == 3):
+        print 'HOURS'
+    elif(len(hms) == 2):
+        print 'ONLY MINS'
+
+def seconds_to_time(seconds):
+    print 'HERE YA GO'
+
+def percent(numerator, denominator):
+    return "%.4f" % (numerator / float(denominator) * 100)
+
+def summary(tribunal_dict):
+    for d in tribunal_dict:
+        count = tribunal_dict[d]
+        print d + ': ' + str(count) + '/' + str(total) + ' (' + (percent(count,total)) + '%)'
 
 json_data = open('./json/data-10.json')
 
@@ -38,17 +52,35 @@ data = json.load(json_data)
 
 total = len(data["tribunal"])
 
-pardonCount = 0
-punishCount = 0
-
 winCount = 0
 lossCount = 0
 
+total_time = 0;
+
+decisionCounts = {}
+agreementCounts = {}
+punishmentCounts = {}
+
 for case in data["tribunal"]:
-    if case["decision"] == "Pardon":
-        pardonCount += 1
-    elif case["decision"] == "Punish":
-        punishCount += 1
+
+    decision = case["decision"]
+    agreement = case["agreement"]
+    punishment = case["punishment"]
+
+    if decision not in decisionCounts:
+        decisionCounts[decision] = 1
+    else:
+        decisionCounts[decision] += 1
+
+    if agreement not in agreementCounts:
+        agreementCounts[agreement] = 1
+    else:
+        agreementCounts[agreement] += 1
+
+    if punishment not in punishmentCounts:
+        punishmentCounts[punishment] = 1
+    else:
+        punishmentCounts[punishment] += 1
 
     for game in case["recentGames"]:
         if game["outcome"] == "Win":
@@ -56,12 +88,25 @@ for case in data["tribunal"]:
         elif game["outcome"] == "Loss":
             lossCount += 1
 
-print str(total) + " tribunal cases total"
-print str(winCount+lossCount) + " games total \n"
+        total_time = time_to_seconds(game["gameLength"])
 
-print str(pardonCount) + "/" + str(total) + " Players pardoned (" + str(pardonCount/float(total)*100) + "%)"
-print str(punishCount) + "/" + str(total) + " Players punished (" + str(punishCount/float(total)*100) + "%)"
+total_games = winCount+lossCount
 
-print "Win rate of reported players: " + str(winCount) + "/" + str(winCount + lossCount) + " (" + str(winCount/float(lossCount + winCount)*100) + "%)"
+print str(total) + 'tribunal cases total'
+print str(total_games) + " games total \n"
+#todo print 'Average game time' + str((total_time / float(total_games)))
+
+print '\nDecisions Summary --'
+summary(decisionCounts)
+
+print '\nAgreements Summary --'
+summary(agreementCounts)
+
+print '\nPunishments Summary --'
+summary(punishmentCounts)
+
+print "\n\nWin rate of reported players: " + str(winCount) + "/" + str(winCount + lossCount) + " (" + (percent(winCount, lossCount + winCount)) + "%)"
 
 json_data.close()
+
+time_to_seconds("22:01:33")
