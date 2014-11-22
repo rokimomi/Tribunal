d6fb07606ee6e3e9e9571ffa124e626ae4f770f2
rokimomi <rokimomi@gmail.com>
2014-11-22 00:53:42 -0500

    Removed stopwords from word cloud

--- Wordcloud.py
+++ Wordcloud.py
@@ -3,14 +3,15 @@ __author__ = 'amine'
 # creates wordcloud from tribunal data
 
 import json
-import time
+from nltk.corpus import stopwords
 
-json_data = open('./json/data-10.json')
+json_data = open('./json/data.json')
 
 data = json.load(json_data)
 
 word_cloud = {}
 word_cloud_reported = {}
+stop_words = set(stopwords.words('english'))
 
 for case in data["tribunal"]:
 
@@ -25,12 +26,21 @@ for case in data["tribunal"]:
 
             player_type = l.split(";@;")[1]
 
-            words = l.split(";@;")[3].split(" ")
+            try:
+                words = l.split(";@;")[3].split(" ")
+
+            except IndexError, e:
+                print "INDEX ERROR HERE: " + l
 
             # general
             for w in words:
 
                 if not len(w) > 0:
+                    continue
+
+                w = w.lower()
+
+                if w in stop_words:
                     continue
 
                 if w not in word_cloud:
@@ -46,19 +56,23 @@ for case in data["tribunal"]:
                         word_cloud_reported[w] += 1
 
 # save data
-with open('./json/word-cloud-10.txt', 'w') as outfile:
+with open('./json/word-cloud.txt', 'w') as outfile:
 
     for w in sorted(word_cloud, key=word_cloud.get, reverse=True):
         try:
+            if word_cloud[w] < 5: continue
+            if not w.isalpha(): continue
             outfile.write(w +":"+ str(word_cloud[w]) + "\n")
         except UnicodeError, e:
             print w +":"+ str(word_cloud[w])
             print "unicode error here, continuing"
 
-with open('./json/word-cloud-reported-10.txt', 'w') as outfile:
+with open('./json/word-cloud-reported.txt', 'w') as outfile:
 
     for w in sorted(word_cloud_reported, key=word_cloud_reported.get, reverse=True):
         try:
+            if word_cloud[w] < 5: continue
+            if not w.isalpha(): continue
             outfile.write(w +":"+ str(word_cloud_reported[w]) + "\n")
         except UnicodeError, e:
             print w +":"+ str(word_cloud_reported[w])
